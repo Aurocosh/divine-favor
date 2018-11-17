37c5ca9c4fab01793f5fe098d02c42ad7e303c0a
Sergei <Antyrox@gmail.com>
2018-11-18 00:07:52 +0700

    test iron medium block added

---
+++ src/main/java/aurocosh/divinefavor/block/BlockImmaterialMedium.java
@@ -0,0 +1,11 @@
+package aurocosh.divinefavor.block;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.util.ResourceLocation;
+
+public abstract class BlockImmaterialMedium extends DivineFavorBlock {
+
+    public BlockImmaterialMedium(ResourceLocation location, Material materialIn) {
+        super(location, materialIn);
+    }
+}
