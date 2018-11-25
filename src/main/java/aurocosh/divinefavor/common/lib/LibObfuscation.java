c631bab2c1229f072dd68f5ec1084ac3f0b8e50a
Aurocosh <Aurocosh@gmail.com>
2018-11-25 21:56:51 +0700

    Basic HUD added

---
+++ src/main/java/aurocosh/divinefavor/common/lib/LibObfuscation.java
@@ -0,0 +1,6 @@
+package aurocosh.divinefavor.common.lib;
+
+public final class LibObfuscation {
+    public static final String[] REMAINING_HIGHLIGHT_TICKS = new String[] { "remainingHighlightTicks", "field_92017_k", "q" };
+
+}
