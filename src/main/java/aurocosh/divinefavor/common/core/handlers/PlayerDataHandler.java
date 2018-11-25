9add18013d350e12cd925fcf02144f1e4cea1c39
Aurocosh <Aurocosh@gmail.com>
2018-11-25 20:21:57 +0700

    fixed data handling

--- src/main/java/aurocosh/divinefavor/common/core/handlers/PlayerDataHandler.java
+++ src/main/java/aurocosh/divinefavor/common/core/handlers/PlayerDataHandler.java
@@ -1,21 +1,22 @@
 package aurocosh.divinefavor.common.core.handlers;
 
-import aurocosh.divinefavor.DivineFavor;
-import aurocosh.divinefavor.common.spell.base.SpellChargeType;
 import aurocosh.divinefavor.common.network.message.MessageDataSync;
 import aurocosh.divinefavor.common.network.message.MessageSyncSpellCharge;
+import aurocosh.divinefavor.common.spell.base.SpellChargeType;
 import aurocosh.divinefavor.common.util.UtilSerialize;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.nbt.*;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
-import vazkii.arl.CommonProxy;
 import vazkii.arl.network.NetworkHandler;
 
 import java.lang.ref.WeakReference;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map.Entry;
 
 public class PlayerDataHandler {
