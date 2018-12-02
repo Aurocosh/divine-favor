aedcc7d257e55a7dc1bfc572e70e832d49286f76
Aurocosh <Aurocosh@gmail.com>
2018-12-02 16:23:29 +0700

    recipes now also consider stack size

---
+++ src/main/java/aurocosh/divinefavor/common/util/helper_classes/ItemStackIdComparator.java
@@ -0,0 +1,15 @@
+package aurocosh.divinefavor.common.util.helper_classes;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import java.util.Comparator;
+
+public class ItemStackIdComparator implements Comparator<ItemStack> {
+    @Override
+    public int compare(ItemStack o1, ItemStack o2) {
+        int id1 = Item.REGISTRY.getIDForObject(o1.getItem());
+        int id2 = Item.REGISTRY.getIDForObject(o2.getItem());
+
+        return id1 - id2;
+    }
+}
