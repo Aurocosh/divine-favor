9c286e0f60b2ff0b47cf01981febd663671d63cc
Aurocosh <Aurocosh@gmail.com>
2018-12-11 01:28:07 +0700

    added spirit romol

--- src/main/java/aurocosh/divinefavor/common/constants/ConstFavorType.java
+++ src/main/java/aurocosh/divinefavor/common/constants/ConstFavorType.java
@@ -1,6 +1,7 @@
 package aurocosh.divinefavor.common.constants;
 
 public final class ConstFavorType {
-    public final static int TIMBER = 1;
-    public final static int ALLFIRE = 2;
+    public final static String FAVOR_OF_TIMBER = "timber";
+    public final static String FAVOR_OF_ALLFIRE = "allfire";
+    public final static String FAVOR_OF_ROMOL = "romol";
 }
