04fb5224f23a9f37528e7b42cc520134abf2cd3e
Aurocosh <Aurocosh@gmail.com>
2018-12-13 22:52:19 +0700

    added searing pulse talisman

--- src/main/java/aurocosh/divinefavor/common/util/UtilBlock.java
+++ src/main/java/aurocosh/divinefavor/common/util/UtilBlock.java
@@ -4,7 +4,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -47,5 +50,33 @@ public class UtilBlock {
         if (particles)
             world.playEvent(2001, pos, Block.getStateId(state));
         return true;
+    }
+
+    public static boolean smeltBlock(EntityPlayer player, World world, BlockPos pos) {
+        if(world.isRemote)
+            return false;
+        if(!world.isBlockModifiable(player, pos))
+            return false;
+
+        IBlockState state = world.getBlockState(pos);
+        Block block = state.getBlock();
+        int meta = block.getMetaFromState(state);
+        ItemStack stack = new ItemStack(block, 1, meta);
+        ItemStack result = FurnaceRecipes.instance().getSmeltingResult(stack);
+        if (result.isEmpty())
+            return false;
+
+        Block blockResult = Block.getBlockFromItem(result.getItem());
+        if (blockResult == Blocks.AIR)
+            return false;
+
+        world.setBlockState(pos, blockResult.getStateFromMeta(result.getMetadata()));
+        state = world.getBlockState(pos);
+        world.playEvent(2001, pos, Block.getStateId(state));
+        return true;
+    }
+
+    public static boolean isAirOrReplaceable(World world, BlockPos pos){
+        return world.isAirBlock(pos) || world.getBlockState(pos).getBlock().isReplaceable(world, pos);
     }
 }
