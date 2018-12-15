7d9c8ebb35fc4af7ae1df5318b87d4448805a7c3
Aurocosh <Aurocosh@gmail.com>
2018-12-15 23:58:35 +0700

    refactored wild sprint talisman

--- src/main/java/aurocosh/divinefavor/common/potions/potions/PotionLiquidWalking.java
+++ src/main/java/aurocosh/divinefavor/common/potions/potions/PotionLiquidWalking.java
@@ -3,9 +3,8 @@ package aurocosh.divinefavor.common.potions.potions;
 import aurocosh.divinefavor.common.potions.base.potion.ModPotion;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
+import static aurocosh.divinefavor.common.util.UtilEntity.tickLiquidWalk;
 
 public class PotionLiquidWalking extends ModPotion {
     private final Block block;
@@ -18,28 +17,6 @@ public class PotionLiquidWalking extends ModPotion {
     @Override
     public void performEffect(EntityLivingBase entityLivingBase, int amplifier) {
         tickLiquidWalk(entityLivingBase, block);
-    }
-
-    private void tickLiquidWalk(EntityLivingBase entityLiving, Block liquid) {
-        World world = entityLiving.getEntityWorld();
-        BlockPos pos = entityLiving.getPosition();
-
-        if (world.getBlockState(pos.down()).getBlock() != liquid)
-            return;
-        if (!world.isAirBlock(pos))
-            return;
-        if(entityLiving.motionY >= 0)
-            return;
-
-        if (entityLiving instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) entityLiving;
-            if (player.isSneaking())
-                return; // let them slip down into it
-        }
-
-        entityLiving.motionY = 0; // stop falling
-        entityLiving.onGround = true; // act as if on solid ground
-        entityLiving.setAIMoveSpeed(0.1F); // walking and not sprinting is this
     }
 
     @Override
--- src/main/java/aurocosh/divinefavor/common/potions/potions/PotionWildSprint.java
+++ src/main/java/aurocosh/divinefavor/common/potions/potions/PotionWildSprint.java
@@ -1,24 +1,38 @@
 package aurocosh.divinefavor.common.potions.potions;
 
+import aurocosh.divinefavor.common.potions.base.potion.ModPotion;
 import aurocosh.divinefavor.common.potions.base.potion.ModPotionTrigger;
 import aurocosh.divinefavor.common.potions.base.effect.PotionEffectCurse;
 import aurocosh.divinefavor.common.util.UtilTick;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.MobEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 
+import java.io.Console;
+
+import static aurocosh.divinefavor.common.util.UtilEntity.tickLiquidWalk;
+
 @Mod.EventBusSubscriber
-public class PotionWildSprint extends ModPotionTrigger {
-    private final int SPEED_DURATION = UtilTick.minutesToTicks(1f);
-    private final int SPEED_LEVEL = 4;
+public class PotionWildSprint extends ModPotion {
+    private final float SPEED_MODIFIER = 0.2f;
+    private final float WATER_RUNNING_SPEED = 0.25f;
 
     public PotionWildSprint() {
         super("wild_sprint", true, 0x7FB8A4);
+        registerPotionAttributeModifier(SharedMonsterAttributes.MOVEMENT_SPEED, "e9c4efd6-98fc-4273-ae05-571f4fd18628", SPEED_MODIFIER, 2);
     }
 
     @Override
-    public void trigger(EntityLivingBase player) {
-        player.addPotionEffect(new PotionEffectCurse(MobEffects.SPEED, SPEED_DURATION, SPEED_LEVEL));
+    public void performEffect(EntityLivingBase entity, int amplifier) {
+        float speedX = (float) Math.abs(entity.motionX);
+        float speedZ = (float) Math.abs(entity.motionZ);
+        if(speedX * speedX + speedZ * speedZ < WATER_RUNNING_SPEED * WATER_RUNNING_SPEED)
+            return;
+        tickLiquidWalk(entity, Blocks.WATER);
     }
 
     @Override
