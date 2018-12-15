3c07bad3b3906f84d9065ce3890b6efb49b46e3b
Sergei <Antyrox@gmail.com>
2018-12-15 16:11:22 +0700

    added spirit squarefury

--- src/main/java/aurocosh/divinefavor/common/item/calling_stones/ModCallingStones.java
+++ src/main/java/aurocosh/divinefavor/common/item/calling_stones/ModCallingStones.java
@@ -8,10 +8,12 @@ public final class ModCallingStones {
     public static ItemCallingStone allfire_calling_stone;
     public static ItemCallingStone timber_calling_stone;
     public static ItemCallingStone romol_calling_stone;
+    public static ItemCallingStone squarefury_calling_stone;
 
     public static void preInit() {
         allfire_calling_stone = ModRegistries.items.register(new ItemCallingStone("allfire", ModSpirits.allfire, ModMultiBlocks.allfire_altar));
         timber_calling_stone = ModRegistries.items.register(new ItemCallingStone("timber", ModSpirits.timber, ModMultiBlocks.timber_altar));
         romol_calling_stone = ModRegistries.items.register(new ItemCallingStone("romol", ModSpirits.romol, ModMultiBlocks.romol_altar));
+        squarefury_calling_stone = ModRegistries.items.register(new ItemCallingStone("squarefury", ModSpirits.squarefury, ModMultiBlocks.squarefury_altar));
     }
 }
