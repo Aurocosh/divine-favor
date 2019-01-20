32bae09152e41c8766bbb110df01ef7528e5cd97
Aurocosh <Aurocosh@gmail.com>
2019-01-20 12:26:47 +0700

    refactored spell arrow

--- src/main/java/aurocosh/divinefavor/common/item/talismans/base/arrow/ArrowType.java
+++ src/main/java/aurocosh/divinefavor/common/item/talismans/base/arrow/ArrowType.java
@@ -1,6 +1,26 @@
 package aurocosh.divinefavor.common.item.talismans.base.arrow;
 
+import aurocosh.divinefavor.common.util.UtilMath;
+
 public enum  ArrowType {
-    SPELL_ARROW,
-    CURSED_ARROW
+    WOODEN_ARROW(0),
+    SPELL_ARROW(1),
+    CURSED_ARROW(2);
+
+    // Optimization
+    public static final ArrowType[] VALUES = ArrowType.values();
+
+    private final int value;
+
+    ArrowType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static ArrowType get(int value){
+        return VALUES[UtilMath.clamp(value, 0, VALUES.length -1 )];
+    }
 }
