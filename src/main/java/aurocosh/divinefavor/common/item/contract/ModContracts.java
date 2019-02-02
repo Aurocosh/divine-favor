e2b4a22dfaae7b1aab034e6e94915bb5163471b8
Aurocosh <Aurocosh@gmail.com>
2019-02-02 17:50:25 +0700

    added contract varieties

--- src/main/java/aurocosh/divinefavor/common/item/contract/ModContracts.java
+++ src/main/java/aurocosh/divinefavor/common/item/contract/ModContracts.java
@@ -13,8 +13,12 @@ public final class ModContracts {
     public static void preInit() {
         Collection<ModFavor> favors = ModRegistries.favors.getValues();
         for (ModFavor favor : favors)
-            contracts.add(new ItemContract("capacity_" + favor.getName(), "capacity/" + favor.getName(), 0, favor, 0, 0, 100));
+            contracts.add(new ItemContract("capacity_minor" + favor.getName(), "capacity/minor/" + favor.getName(), 0, favor, 0, 0, 100));
         for (ModFavor favor : favors)
-            contracts.add(new ItemContract("regen_" + favor.getName(), "regen/" + favor.getName(), 1, favor, 20, 0, 0));
+            contracts.add(new ItemContract("capacity_major" + favor.getName(), "capacity/major/" + favor.getName(), 0, favor, 0, 0, 500));
+        for (ModFavor favor : favors)
+            contracts.add(new ItemContract("regen_minor_" + favor.getName(), "regen/minor/" + favor.getName(), 1, favor, 20, 0, 0));
+        for (ModFavor favor : favors)
+            contracts.add(new ItemContract("regen_major_" + favor.getName(), "regen/major/" + favor.getName(), 1, favor, 200, 0, 0));
     }
 }
