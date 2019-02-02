3ca5cbb9fb6690f7f9ba606a42bc59ba2163a726
Sergei <Antyrox@gmail.com>
2019-02-02 17:34:08 +0700

    refactoring

--- src/main/java/aurocosh/divinefavor/common/core/creative_tabs/DivineFavorCreativeTabArrowTalismans.java
+++ src/main/java/aurocosh/divinefavor/common/core/creative_tabs/DivineFavorCreativeTabArrowTalismans.java
@@ -45,6 +45,7 @@ public class DivineFavorCreativeTabArrowTalismans extends CreativeTabs {
 
         ModRegistries.blocks.getValues().forEach(this::addBlock);
         ModRegistries.items.getValues().forEach(this::addItem);
+        stacks.sort(new ModItemStackComparator());
     }
 
     private void addItem(Item item) {
--- src/main/java/aurocosh/divinefavor/common/core/creative_tabs/DivineFavorCreativeTabSpellTalismans.java
+++ src/main/java/aurocosh/divinefavor/common/core/creative_tabs/DivineFavorCreativeTabSpellTalismans.java
@@ -45,6 +45,7 @@ public class DivineFavorCreativeTabSpellTalismans extends CreativeTabs {
 
         ModRegistries.blocks.getValues().forEach(this::addBlock);
         ModRegistries.items.getValues().forEach(this::addItem);
+        stacks.sort(new ModItemStackComparator());
     }
 
     private void addItem(Item item) {
