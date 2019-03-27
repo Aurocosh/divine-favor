c4d3e5b04a1b060bbcdf38963e59d4d7a004f113
Aurocosh <Aurocosh@gmail.com>
2019-03-27 20:25:43 +0700

    refactoring

--- src/main/java/aurocosh/divinefavor/common/config/common/ConfigArrow.java
+++ src/main/java/aurocosh/divinefavor/common/config/common/ConfigArrow.java
@@ -5,7 +5,7 @@ import aurocosh.divinefavor.common.constants.ConstMisc;
 import net.minecraftforge.common.config.Config;
 
 @Config(modid = ConstMisc.MOD_ID, name = ConstMisc.MOD_ID + "_arrow_talismans")
-public class ConfigArrowTalismans {
+public class ConfigArrow {
     @Config.Name("Fins")
     public static Fins fins = new Fins();
 }
