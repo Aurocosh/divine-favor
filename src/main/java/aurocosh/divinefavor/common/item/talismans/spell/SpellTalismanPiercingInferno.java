7a541d076d4a5252906478faf5dceb28669791f3
Aurocosh <Aurocosh@gmail.com>
2019-03-31 20:00:57 +0700

    reworked piercing inferno talisman

--- src/main/java/aurocosh/divinefavor/common/item/talismans/spell/SpellTalismanPiercingInferno.java
+++ src/main/java/aurocosh/divinefavor/common/item/talismans/spell/SpellTalismanPiercingInferno.java
@@ -6,11 +6,15 @@ import aurocosh.divinefavor.common.item.talismans.spell.base.ItemSpellTalisman;
 import aurocosh.divinefavor.common.item.talismans.spell.base.SpellOptions;
 import aurocosh.divinefavor.common.item.talismans.spell.base.TalismanContext;
 import aurocosh.divinefavor.common.util.UtilCoordinates;
+import aurocosh.divinefavor.common.util.UtilList;
 import aurocosh.divinefavor.common.util.UtilRandom;
+import aurocosh.divinefavor.common.util.tasks.BlockProcessingTask;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.MinecraftForge;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -21,14 +25,25 @@ public class SpellTalismanPiercingInferno extends ItemSpellTalisman {
 
     @Override
     protected void performActionServer(TalismanContext context) {
-        List<BlockPos> piercingShape = UtilCoordinates.getNeighboursWithSameExposedFace(context.pos, context.world, context.facing, ConfigSpells.piercingInferno.maxPierceShapeSize);
+        int blocksToBreak = context.player.isSneaking() ? ConfigSpells.piercingInferno.blocksToBreakWeak : ConfigSpells.piercingInferno.blocksToBreakNormal;
+        List<BlockPos> piercingShape = UtilCoordinates.getNeighboursWithSameExposedFace(context.pos, context.world, context.facing, blocksToBreak);
+        blocksToBreak -= piercingShape.size();
+
         BlockPos shift = new BlockPos(context.facing.getOpposite().getDirectionVec());
-        for (int i = 0; i < ConfigSpells.piercingInferno.maxPierceDepth; i++) {
-            for (BlockPos pos : piercingShape) {
-                Block block = UtilRandom.rollDice(ConfigSpells.piercingInferno.chanceToIgnite) ? Blocks.FIRE : Blocks.AIR;
-                context.world.setBlockState(pos, block.getDefaultState());
+        List<BlockPos> blocksToRemove = new ArrayList<>();
+
+        while (blocksToBreak > 0) {
+            for (int i = 0; i < piercingShape.size() && blocksToBreak-- > 0; i++) {
+                BlockPos pos = piercingShape.get(i);
+                blocksToRemove.add(new BlockPos(pos));
             }
             piercingShape = UtilCoordinates.shiftCoordinates(piercingShape, shift);
         }
+
+        UtilList.Processor<BlockPos> processor = pos -> {
+            Block block = UtilRandom.rollDice(ConfigSpells.piercingInferno.chanceToIgnite) ? Blocks.FIRE : Blocks.AIR;
+            context.world.setBlockState(pos, block.getDefaultState());
+        };
+        MinecraftForge.EVENT_BUS.register(new BlockProcessingTask(blocksToRemove, context.world, 1, processor));
     }
 }
