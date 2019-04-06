86baeb7e26201c94c34152e3f7a4d6355e4fde74
Sergei <Antyrox@gmail.com>
2019-04-06 22:29:29 +0700

    added starting favor values config

--- src/main/java/aurocosh/divinefavor/common/custom_data/player/data/favor/FavorStatus.java
+++ src/main/java/aurocosh/divinefavor/common/custom_data/player/data/favor/FavorStatus.java
@@ -1,17 +1,18 @@
 package aurocosh.divinefavor.common.custom_data.player.data.favor;
 
+import aurocosh.divinefavor.common.favor.ModFavor;
 import aurocosh.divinefavor.common.item.contract.ItemContract;
 import net.minecraft.util.math.MathHelper;
 
 public class FavorStatus {
+    private final ModFavor favor;
     private int regen;
     private int minLimit;
     private int maxLimit;
 
-    public FavorStatus() {
-        regen = 0;
-        minLimit = 0;
-        maxLimit = 0;
+    public FavorStatus(ModFavor favor) {
+        this.favor = favor;
+        reset();
     }
 
     public int getRegen() {
@@ -26,10 +27,10 @@ public class FavorStatus {
         return maxLimit;
     }
 
-    public void clear() {
-        regen = 0;
-        minLimit = 0;
-        maxLimit = 0;
+    public void reset() {
+        regen = favor.getFavorRegen();
+        minLimit = favor.getFavorMin();
+        maxLimit = favor.getFavorMax();
     }
 
     public void addStats(ItemContract contract) {
--- src/main/java/aurocosh/divinefavor/common/favor/ModFavor.java
+++ src/main/java/aurocosh/divinefavor/common/favor/ModFavor.java
@@ -1,5 +1,6 @@
 package aurocosh.divinefavor.common.favor;
 
+import aurocosh.divinefavor.common.config.FavorConfig;
 import aurocosh.divinefavor.common.constants.ConstResources;
 import aurocosh.divinefavor.common.core.ResourceNamer;
 import aurocosh.divinefavor.common.lib.interfaces.IIndexedEntry;
@@ -13,12 +14,20 @@ public class ModFavor extends IForgeRegistryEntry.Impl<ModFavor> implements IInd
     private final String name;
     private final ResourceLocation icon;
     private final ResourceLocation symbol;
-    // Talisman functions
 
-    public ModFavor(String name) {
+    private final int favorMin;
+    private final int favorMax;
+    private final int favorRegen;
+
+    // Talisman functions
+    public ModFavor(String name, FavorConfig favorConfig) {
         this.name = name;
         icon = new ResourceLocation(ConstResources.PREFIX_FAVOR_ICONS + name + ".png");
         symbol = new ResourceLocation(ConstResources.PREFIX_FAVOR_SYMBOLS + name + ".png");
+
+        favorMin = favorConfig.startingPlayerFavorMinimum;
+        favorMax = favorConfig.startingPlayerFavorMaximum;
+        favorRegen = favorConfig.startingPlayerFavorRegen;
 
         setRegistryName(ResourceNamer.getFullName("favor", name));
         id = ModMappers.favors.register(this);
@@ -35,6 +44,18 @@ public class ModFavor extends IForgeRegistryEntry.Impl<ModFavor> implements IInd
 
     public ResourceLocation getSymbol() {
         return symbol;
+    }
+
+    public int getFavorMin() {
+        return favorMin;
+    }
+
+    public int getFavorMax() {
+        return favorMax;
+    }
+
+    public int getFavorRegen() {
+        return favorRegen;
     }
 
     @Override
