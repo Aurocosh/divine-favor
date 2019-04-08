af554068d234a88d4ca28e380dfacf0df4de98ae
Aurocosh <Aurocosh@gmail.com>
2019-04-08 23:31:21 +0700

    added squarefury punishment config

--- src/main/java/aurocosh/divinefavor/common/config/common/ConfigPunishments.java
+++ src/main/java/aurocosh/divinefavor/common/config/common/ConfigPunishments.java
@@ -1,9 +1,6 @@
 package aurocosh.divinefavor.common.config.common;
 
-import aurocosh.divinefavor.common.config.punishment.BlizrabiPunishmentConfig;
-import aurocosh.divinefavor.common.config.punishment.EnderererPunishmentConfig;
-import aurocosh.divinefavor.common.config.punishment.LoonPunishmentConfig;
-import aurocosh.divinefavor.common.config.punishment.NeblazePunishmentConfig;
+import aurocosh.divinefavor.common.config.punishment.*;
 import aurocosh.divinefavor.common.constants.ConstMisc;
 import net.minecraftforge.common.config.Config;
 
@@ -17,4 +14,6 @@ public class ConfigPunishments {
     public static LoonPunishmentConfig loon = new LoonPunishmentConfig();
     @Config.Name("Neblaze")
     public static NeblazePunishmentConfig neblaze = new NeblazePunishmentConfig();
+    @Config.Name("Squarefury")
+    public static SquarefuryPunishmentConfig squarefury = new SquarefuryPunishmentConfig();
 }
