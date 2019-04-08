45d35cbb5f6e66075c4b91546c8e9bbaefe835fc
Sergei <Antyrox@gmail.com>
2019-04-08 23:31:21 +0700

    added squarefury punishment config

--- src/main/java/aurocosh/divinefavor/common/spirit/punishment/SquareFuryPunishment.java
+++ src/main/java/aurocosh/divinefavor/common/spirit/punishment/SquareFuryPunishment.java
@@ -1,5 +1,6 @@
 package aurocosh.divinefavor.common.spirit.punishment;
 
+import aurocosh.divinefavor.common.config.common.ConfigPunishments;
 import aurocosh.divinefavor.common.damage_source.ModDamageSources;
 import aurocosh.divinefavor.common.muliblock.instance.MultiBlockInstance;
 import aurocosh.divinefavor.common.spirit.base.SpiritPunishment;
@@ -9,10 +10,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class SquareFuryPunishment extends SpiritPunishment {
-    public static float DAMAGE = 10;
-
     @Override
     public void execute(EntityPlayer player, World world, BlockPos pos, IBlockState state, MultiBlockInstance instance) {
-        player.attackEntityFrom(ModDamageSources.divineDamage, DAMAGE);
+        player.attackEntityFrom(ModDamageSources.divineDamage, ConfigPunishments.squarefury.damage);
     }
 }
