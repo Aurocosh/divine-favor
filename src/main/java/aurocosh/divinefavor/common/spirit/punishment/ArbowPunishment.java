a8bd9e5a4b5253bb175626fe1609b4f20b2e2f7c
Aurocosh <Aurocosh@gmail.com>
2019-04-09 22:09:07 +0700

    added arbow punishment

--- src/main/java/aurocosh/divinefavor/common/spirit/punishment/ArbowPunishment.java
+++ src/main/java/aurocosh/divinefavor/common/spirit/punishment/ArbowPunishment.java
@@ -3,6 +3,8 @@ package aurocosh.divinefavor.common.spirit.punishment;
 import aurocosh.divinefavor.common.config.common.ConfigPunishments;
 import aurocosh.divinefavor.common.damage_source.ModDamageSources;
 import aurocosh.divinefavor.common.muliblock.instance.MultiBlockInstance;
+import aurocosh.divinefavor.common.potions.base.effect.ModEffect;
+import aurocosh.divinefavor.common.potions.common.ModCurses;
 import aurocosh.divinefavor.common.spirit.base.SpiritPunishment;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,5 +15,7 @@ public class ArbowPunishment extends SpiritPunishment {
     @Override
     public void execute(EntityPlayer player, World world, BlockPos pos, IBlockState state, MultiBlockInstance instance) {
         player.attackEntityFrom(ModDamageSources.divineDamage, ConfigPunishments.arbow.damage);
+        player.addPotionEffect(new ModEffect(ModCurses.yummy_smell, ConfigPunishments.arbow.curseDuration.random()));
+        player.addPotionEffect(new ModEffect(ModCurses.cripple, ConfigPunishments.arbow.curseDuration.random()));
     }
 }
