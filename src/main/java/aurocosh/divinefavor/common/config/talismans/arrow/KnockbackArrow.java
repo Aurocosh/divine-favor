41108d6451cb842ac0bbeb07ea5f5a9edced741a
Sergei <Antyrox@gmail.com>
2019-04-10 00:41:47 +0700

    added knockback arrow

---
+++ src/main/java/aurocosh/divinefavor/common/config/talismans/arrow/KnockbackArrow.java
@@ -0,0 +1,12 @@
+package aurocosh.divinefavor.common.config.talismans.arrow;
+
+import net.minecraftforge.common.config.Config;
+
+public class KnockbackArrow {
+    @Config.Name("Favor cost")
+    public int favorCost = 20;
+    @Config.Name("Damage")
+    public float damage = 0.5f;
+    @Config.Name("Damage")
+    public float knockback = 2;
+}
--- src/main/java/aurocosh/divinefavor/common/item/talismans/arrow/ArrowTalismanKnockbackArrow.java
+++ src/main/java/aurocosh/divinefavor/common/item/talismans/arrow/ArrowTalismanKnockbackArrow.java
@@ -7,20 +7,19 @@ import aurocosh.divinefavor.common.spirit.base.ModSpirit;
 import aurocosh.divinefavor.common.util.UtilEntity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
-import net.minecraft.util.math.BlockPos;
 
 import java.util.EnumSet;
 
-public class ArrowTalismanNetherSwap extends ItemArrowTalisman {
-    public ArrowTalismanNetherSwap(String name, ModSpirit spirit, int favorCost, int color, double arrowDamage, ArrowType arrowType) {
+public class ArrowTalismanKnockbackArrow extends ItemArrowTalisman {
+    private final float knockback;
+
+    public ArrowTalismanKnockbackArrow(String name, ModSpirit spirit, int favorCost, int color, double arrowDamage, ArrowType arrowType, float knockback) {
         super(name, spirit, favorCost, color, arrowDamage, EnumSet.of(ArrowOptions.BreakOnHit, ArrowOptions.RequiresTarget), arrowType);
+        this.knockback = knockback;
     }
 
     @Override
     protected void performActionServer(EntityLivingBase target, EntityLivingBase shooter, EntityArrow arrow) {
-        BlockPos targetPos = target.getPosition();
-        BlockPos shooterPos = shooter.getPosition();
-        UtilEntity.teleport(shooter, targetPos);
-        UtilEntity.teleport(target, shooterPos);
+        UtilEntity.addVelocity(target, arrow.getLookVec(), knockback);
     }
 }
