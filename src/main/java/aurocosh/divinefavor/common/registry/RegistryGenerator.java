f6bcbc21b2480897f556b37b15f938936b22e121
Sergei <Antyrox@gmail.com>
2019-05-03 00:32:31 +0700

    fixed registry creation

---
+++ src/main/java/aurocosh/divinefavor/common/registry/RegistryGenerator.java
@@ -0,0 +1,27 @@
+package aurocosh.divinefavor.common.registry;
+
+import aurocosh.divinefavor.common.constants.ConstMisc;
+import aurocosh.divinefavor.common.core.ResourceNamer;
+import aurocosh.divinefavor.common.muliblock.ModMultiBlock;
+import aurocosh.divinefavor.common.spirit.base.ModSpirit;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.RegistryBuilder;
+// Does not work for some reason if converted to kotlin
+@Mod.EventBusSubscriber(modid = ConstMisc.MOD_ID)
+public final class RegistryGenerator {
+    private final static String REGESTRY_PREFIX = "registry";
+
+    @SubscribeEvent
+    public static void onCreateRegistry(RegistryEvent.NewRegistry event) {
+        new RegistryBuilder<ModMultiBlock>()
+                .setType(ModMultiBlock.class)
+                .setName(ResourceNamer.INSTANCE.getFullName(REGESTRY_PREFIX,"multi_blocks"))
+                .create();
+        new RegistryBuilder<ModSpirit>()
+                .setType(ModSpirit.class)
+                .setName(ResourceNamer.INSTANCE.getFullName(REGESTRY_PREFIX,"spirits"))
+                .create();
+    }
+}
