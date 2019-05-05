24d2dcac90dc4e61ba82417fed0b42c0fe3b3f55
Sergei <Antyrox@gmail.com>
2019-05-05 23:01:24 +0700

    minor fixes

--- src/main/kotlin/aurocosh/divinefavor/common/lib/distributed_random/DistributedRandomEntityList.kt
+++ src/main/kotlin/aurocosh/divinefavor/common/lib/distributed_random/DistributedRandomEntityList.kt
@@ -2,17 +2,24 @@ package aurocosh.divinefavor.common.lib.distributed_random
 
 import aurocosh.divinefavor.DivineFavor
 import net.minecraft.entity.Entity
-import net.minecraft.entity.EntityList
+import net.minecraft.util.ResourceLocation
 
 class DistributedRandomEntityList(entityTypeMap: Map<String, Double>) : DistributedRandomList<Class<out Entity>>() {
 
     init {
         for ((entityName, value) in entityTypeMap) {
-            val entityClass = EntityList.getClassFromName(entityName)
+            val entityClass = getClassFromName(entityName)
             if (entityClass != null)
                 add(entityClass, value)
             else
                 DivineFavor.logger.error("Entity type not found: $entityName")
+        }
+    }
+
+    companion object {
+        fun getClassFromName(p_192839_0_: String): Class<out Entity>? {
+            val entry = net.minecraftforge.fml.common.registry.ForgeRegistries.ENTITIES.getValue(ResourceLocation(p_192839_0_))
+            return entry?.entityClass
         }
     }
 }
