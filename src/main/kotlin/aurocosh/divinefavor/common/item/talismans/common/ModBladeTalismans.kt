e07d4e2d1ed23c162064d7c30c1e45ab79159019
Aurocosh <Aurocosh@gmail.com>
2019-06-12 03:52:06 +0700

    marker refactoring

--- src/main/kotlin/aurocosh/divinefavor/common/item/talismans/common/ModBladeTalismans.kt
+++ src/main/kotlin/aurocosh/divinefavor/common/item/talismans/common/ModBladeTalismans.kt
@@ -24,7 +24,7 @@ object ModBladeTalismans {
     lateinit var vengeful_blade: ItemBladeTalisman
     lateinit var wither_coating: ItemBladeTalisman
 
-    // New talisman vars
+    // New fields
     lateinit var yummy_smell: ItemBladeTalisman
     lateinit var wind_leash: ItemBladeTalisman
     lateinit var suffocating_fumes: ItemBladeTalisman
@@ -68,7 +68,7 @@ object ModBladeTalismans {
         // timber;
         wither_coating = BladeTalismanWitherCoating("wither_coating", ModSpirits.timber, ConfigBlade.witherCoating.favorCost)
 
-        // New talismans
+        // New instances
         yummy_smell = BladeTalismanModPotion("yummy_smell", ModSpirits.timber, ConfigBlade.yummySmell.favorCost, ModCurses.yummy_smell, ConfigBlade.yummySmell.duration)
         wind_leash = BladeTalismanModPotion("wind_leash", ModSpirits.timber, ConfigBlade.windLeash.favorCost, ModCurses.wind_leash, ConfigBlade.windLeash.duration)
         suffocating_fumes = BladeTalismanModPotion("suffocating_fumes", ModSpirits.timber, ConfigBlade.suffocatingFumes.favorCost, ModCurses.suffocating_fumes, ConfigBlade.suffocatingFumes.duration)
