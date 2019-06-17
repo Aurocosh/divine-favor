d3c6d2745cc6c7587cb7015d412736f285e7fd07
Aurocosh <Aurocosh@gmail.com>
2019-06-18 01:19:36 +0700

    added teleportation markers to blink and warp talismans

--- src/main/kotlin/aurocosh/divinefavor/common/item/spell_talismans/SpellTalismanBlink.kt
+++ src/main/kotlin/aurocosh/divinefavor/common/item/spell_talismans/SpellTalismanBlink.kt
@@ -4,12 +4,23 @@ import aurocosh.divinefavor.common.config.common.ConfigSpell
 import aurocosh.divinefavor.common.item.spell_talismans.base.ItemSpellTalisman
 import aurocosh.divinefavor.common.item.spell_talismans.base.SpellOptions
 import aurocosh.divinefavor.common.item.spell_talismans.context.TalismanContext
+import aurocosh.divinefavor.common.lib.extensions.toBlockPos
+import aurocosh.divinefavor.common.lib.extensions.toVec3d
+import aurocosh.divinefavor.common.particles.ModParticles
+import aurocosh.divinefavor.common.particles.particles.ImmobileParticle
 import aurocosh.divinefavor.common.stack_properties.StackPropertyInt
 import aurocosh.divinefavor.common.spirit.base.ModSpirit
 import aurocosh.divinefavor.common.util.UtilEntity
+import aurocosh.divinefavor.common.util.UtilRandom
 import net.minecraft.item.ItemStack
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
+import net.minecraftforge.client.event.RenderWorldLastEvent
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import java.awt.Color
 import java.util.*
+import javax.vecmath.Color3f
 
 class SpellTalismanBlink(name: String, spirit: ModSpirit, favorCost: Int, options: EnumSet<SpellOptions>) : ItemSpellTalisman(name, spirit, favorCost, options) {
     private val blinkDistance: StackPropertyInt = propertyHandler.registerIntProperty("blink_distance", ConfigSpell.blink.maxDistance)
@@ -20,20 +31,37 @@ class SpellTalismanBlink(name: String, spirit: ModSpirit, favorCost: Int, option
 
     public override fun validate(context: TalismanContext): Boolean {
         val world = context.world
-        val target = getBlinkTarget(context)
-        return world.isAirBlock(target) && world.isAirBlock(target.up())
+        val target = getBlinkTarget(context, 0.0)
+        val pos = target.toBlockPos()
+        return world.isAirBlock(pos) && world.isAirBlock(pos)
     }
 
     override fun performActionServer(context: TalismanContext) {
-        val target = getBlinkTarget(context)
-        UtilEntity.teleport(context.player, target.down())
+        val target = getBlinkTarget(context, 0.0)
+        UtilEntity.teleport(context.player, target)
     }
 
-    private fun getBlinkTarget(context: TalismanContext): BlockPos {
+    private fun getBlinkTarget(context: TalismanContext, shift: Double): Vec3d {
         val player = context.player
-        val pos = player.getPositionEyes(0f)
+        val pos = player.positionVector
         val look = player.lookVec
         val distance = blinkDistance.getValue(context.stack)
-        return BlockPos(pos.add(look.scale(distance.toDouble())))
+        return pos.add(look.scale(distance.toDouble())).add(0.0, shift, 0.0)
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun handleRendering(context: TalismanContext, lastEvent: RenderWorldLastEvent) {
+        val target = getBlinkTarget(context, context.player.eyeHeight.toDouble())
+        val isAir = context.world.isAirBlock(target.toBlockPos())
+        if (!isAir)
+            return
+
+        val nextRadius = UtilRandom.nextDouble(0.2, 1.2)
+        val shift = UtilRandom.nextDirection().scale(nextRadius)
+        ModParticles.normal.createParticle(target) { ImmobileParticle(context.world, target.add(shift), Color3f(particleColor), 3) }
+    }
+
+    companion object {
+        val particleColor = Color.MAGENTA.darker()
     }
 }
--- src/main/kotlin/aurocosh/divinefavor/common/item/spell_talismans/SpellTalismanWarp.kt
+++ src/main/kotlin/aurocosh/divinefavor/common/item/spell_talismans/SpellTalismanWarp.kt
@@ -4,12 +4,20 @@ import aurocosh.divinefavor.common.config.common.ConfigSpell
 import aurocosh.divinefavor.common.item.spell_talismans.base.ItemSpellTalisman
 import aurocosh.divinefavor.common.item.spell_talismans.base.SpellOptions
 import aurocosh.divinefavor.common.item.spell_talismans.context.TalismanContext
+import aurocosh.divinefavor.common.particles.ModParticles
+import aurocosh.divinefavor.common.particles.particles.ImmobileParticle
 import aurocosh.divinefavor.common.stack_properties.StackPropertyInt
 import aurocosh.divinefavor.common.spirit.base.ModSpirit
 import aurocosh.divinefavor.common.util.UtilEntity
+import aurocosh.divinefavor.common.util.UtilRandom
 import net.minecraft.item.ItemStack
-import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
+import net.minecraftforge.client.event.RenderWorldLastEvent
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import java.awt.Color
 import java.util.*
+import javax.vecmath.Color3f
 
 class SpellTalismanWarp(name: String, spirit: ModSpirit, favorCost: Int, options: EnumSet<SpellOptions>) : ItemSpellTalisman(name, spirit, favorCost, options) {
     private val blinkDistance: StackPropertyInt = propertyHandler.registerIntProperty("blink_distance", ConfigSpell.warp.maxDistance)
@@ -19,10 +27,27 @@ class SpellTalismanWarp(name: String, spirit: ModSpirit, favorCost: Int, options
     }
 
     override fun performActionServer(context: TalismanContext) {
-        val pos = context.player.getPositionEyes(0f)
+        val target = getTarget(context, 0.0)
+        UtilEntity.teleport(context.player, target)
+    }
+
+    private fun getTarget(context: TalismanContext, shift: Double): Vec3d {
+        val pos = context.player.positionVector
         val look = context.player.lookVec
         val distance = blinkDistance.getValue(context.stack)
-        val target = BlockPos(pos.add(look.scale(distance.toDouble())))
-        UtilEntity.teleport(context.player, target.down())
+        val target = pos.add(look.scale(distance.toDouble())).add(0.0, shift, 0.0)
+        return target
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun handleRendering(context: TalismanContext, lastEvent: RenderWorldLastEvent) {
+        val target = getTarget(context, context.player.eyeHeight.toDouble())
+        val nextRadius = UtilRandom.nextDouble(0.2, 1.2)
+        val shift = UtilRandom.nextDirection().scale(nextRadius)
+        ModParticles.noDepth.createParticle(target) { ImmobileParticle(context.world, target.add(shift), Color3f(particleColor), 3) }
+    }
+
+    companion object {
+        val particleColor = Color.RED.darker()
     }
 }
