40d0093e74840941c3a51187ff9710d88db8359f
Aurocosh <Aurocosh@gmail.com>
2019-06-18 19:43:38 +0700

    fixed talisman property sync

--- src/main/kotlin/aurocosh/divinefavor/common/network/message/sever/talisman_properties/MessageSyncTalismanProperty.kt
+++ src/main/kotlin/aurocosh/divinefavor/common/network/message/sever/talisman_properties/MessageSyncTalismanProperty.kt
@@ -1,6 +1,8 @@
 package aurocosh.divinefavor.common.network.message.sever.talisman_properties
 
+import aurocosh.divinefavor.common.item.talisman.ITalismanContainer
 import aurocosh.divinefavor.common.item.talisman.ItemTalisman
+import aurocosh.divinefavor.common.item.talisman_tools.ItemTalismanContainer
 import aurocosh.divinefavor.common.network.message.base.DivineServerMessage
 import aurocosh.divinefavor.common.stack_properties.StackProperty
 import aurocosh.divinefavor.common.util.UtilPlayer
@@ -20,10 +22,20 @@ abstract class MessageSyncTalismanProperty<T> : DivineServerMessage {
     }
 
     override fun handleSafe(serverPlayer: EntityPlayerMP) {
-        val stack = UtilPlayer.getItemInHand(serverPlayer) { it is ItemTalisman && it.properties.exist(name) }
-        val talisman = stack.item as ItemTalisman
-        val property = talisman.properties.get(name) as? StackProperty<T> ?: return
+        val stack = UtilPlayer.getItemInHand(serverPlayer) { it is ItemTalismanContainer }
+        if(stack.isEmpty)
+            return
 
+        val container = stack.item as ITalismanContainer
+        val talismanStack = container.getTalismanStack(stack)
+        if(talismanStack.isEmpty)
+            return
+
+        val talisman = talismanStack.item as ItemTalisman
+        if(!talisman.properties.exist(name))
+            return
+
+        val property = talisman.properties.get(name) as? StackProperty<T> ?: return
         property.setValue(stack, value)
     }
 }
