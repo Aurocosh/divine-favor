afbc217246157c1abc4debf66efab03a5b6da613
Aurocosh <Aurocosh@gmail.com>
2019-06-22 22:06:27 +0700

    fixed recipe collision check and removed invalid recipes

--- src/main/kotlin/aurocosh/divinefavor/common/receipes/ImmaterialMediumRecipe.kt
+++ src/main/kotlin/aurocosh/divinefavor/common/receipes/ImmaterialMediumRecipe.kt
@@ -7,29 +7,27 @@ import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.Ingredient
 import net.minecraft.util.ResourceLocation
-import java.util.*
-import kotlin.collections.ArrayList
 
 class ImmaterialMediumRecipe(val result: ItemStack, val callingStones: List<Ingredient>, val ingredients: List<Ingredient>) {
     val name: ResourceLocation
         get() = result.item.registryName ?: emptyLocation()
 
     private val acceptableStones = callingStones.map { it.getMatchingStacks().first().item }.toSet()
-    private val possibleItemIds = ingredients.flatMap{ it.getMatchingStacks().asIterable() }.map { Item.getIdFromItem(it.item) }.toSet()
+    private val possibleItemIds = ingredients.flatMap { it.getMatchingStacks().asIterable() }.map { Item.getIdFromItem(it.item) }.toSet()
 
     fun isMatching(callingStone: ItemCallingStone, stacks: List<ItemStack>): Boolean {
-        if(stacks.size != ingredients.size)
+        if (stacks.size != ingredients.size)
             return false
-        if(!acceptableStones.contains(callingStone))
+        if (!acceptableStones.contains(callingStone))
             return false
         val itemIdSet = stacks.map { Item.getIdFromItem(it.item) }.toSet()
-        if(!possibleItemIds.containsAll(itemIdSet))
+        if (!possibleItemIds.containsAll(itemIdSet))
             return false
 
         val requiredIngredients = ingredients.toMutableList()
         for (stack in stacks) {
             val match = requiredIngredients.firstOrNull { it.apply(stack) }
-            if(match != null)
+            if (match != null)
                 requiredIngredients.remove(match)
         }
         return requiredIngredients.isEmpty()
@@ -37,8 +35,8 @@ class ImmaterialMediumRecipe(val result: ItemStack, val callingStones: List<Ingr
 
     fun getStringId(): String {
         return ingredients.S
-                .map { it.getMatchingStacks() }
-                .map { it.joinToString("_") { stack -> Item.getIdFromItem(stack.item).toString() } }
-                .joinToString { "-" }
+                .map { it.getMatchingStacks() }.toList()
+                .map { it.joinToString("_") { stack -> Item.getIdFromItem(stack.item).toString() + "_" + stack.metadata } }.toList()
+                .joinToString(",")
     }
 }
