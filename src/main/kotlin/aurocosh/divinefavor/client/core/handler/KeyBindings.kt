2073e6776f0d950e7972070313da7795aa5deeca
Aurocosh <Aurocosh@gmail.com>
2019-12-05 18:41:32 +0700

    Fixed keybindings

--- src/main/kotlin/aurocosh/divinefavor/client/core/handler/KeyBindings.kt
+++ src/main/kotlin/aurocosh/divinefavor/client/core/handler/KeyBindings.kt
@@ -1,7 +1,6 @@
 package aurocosh.divinefavor.client.core.handler
 
 import aurocosh.divinefavor.DivineFavor
-import aurocosh.divinefavor.common.core.ResourceNamer
 import aurocosh.divinefavor.common.item.talisman.ITalismanToolContainer
 import aurocosh.divinefavor.common.item.talisman.ItemTalisman
 import aurocosh.divinefavor.common.util.UtilPlayer
@@ -14,53 +13,48 @@ import org.lwjgl.input.Keyboard
 
 @SideOnly(Side.CLIENT)
 object KeyBindings {
-    private val CONFLICT_CONTEXT = KeyConflictContext()
-    private val TEST_CONFLICT_CONTEXT = TestKeyConflictContext()
+    private val CONTAINER_CONFLICT_CONTEXT = ContainerKeyConflictContext()
+    private val TALISMAN_CONFLICT_CONTEXT = TalismanKeyConflictContext()
+
     lateinit var talismanScroll: KeyBinding
     lateinit var talismanSelect: KeyBinding
     lateinit var talismanValueHud: KeyBinding
 //    lateinit var ropeKey: KeyBinding
 
     fun init() {
-        talismanScroll = createBinding("talisman_scroll", Keyboard.KEY_Y, CONFLICT_CONTEXT)
-        talismanSelect = createBinding("talisman_select", Keyboard.KEY_V, CONFLICT_CONTEXT)
-        talismanValueHud = createBinding("talisman_value_hud", Keyboard.KEY_R, TEST_CONFLICT_CONTEXT)
+        talismanScroll = createBinding("talisman_scroll", Keyboard.KEY_Y, CONTAINER_CONFLICT_CONTEXT)
+        talismanSelect = createBinding("talisman_select", Keyboard.KEY_V, CONTAINER_CONFLICT_CONTEXT)
+        talismanValueHud = createBinding("talisman_value_hud", Keyboard.KEY_R, TALISMAN_CONFLICT_CONTEXT)
 //        ropeKey = createBinding("rope_key", Keyboard.KEY_N)
     }
 
     private fun createBinding(name: String, key: Int, conflictContext: IKeyConflictContext): KeyBinding {
-        val keyBinding = KeyBinding("key." + ResourceNamer.getFullName(name), conflictContext, key, DivineFavor.KEYBIND_CATEGORY)
+        val keyBinding = KeyBinding("key.${DivineFavor.MOD_ID}.$name", conflictContext, key, DivineFavor.KEYBIND_CATEGORY)
         ClientRegistry.registerKeyBinding(keyBinding)
         return keyBinding
     }
 
-    class KeyConflictContext : IKeyConflictContext {
+    class ContainerKeyConflictContext : IKeyConflictContext {
         override fun isActive(): Boolean {
-            val player = DivineFavor.proxy.clientPlayer
-//            if (!net.minecraftforge.client.settings.KeyConflictContext.GUI.isActive())
-            //                return false;
-            return UtilPlayer.getHandWithItem(player) { it is ITalismanToolContainer } != null
+            if (!DivineFavor.proxy.hasClientPlayer)
+                return false
+            return UtilPlayer.getHandWithItem(DivineFavor.proxy.clientPlayer) { it is ITalismanToolContainer } != null
         }
 
         override fun conflicts(other: IKeyConflictContext): Boolean {
-            return false
-            //            return other == this;
-            //            return other == this || other == net.minecraftforge.client.settings.KeyConflictContext.IN_GAME;
+            return other == this || other == TALISMAN_CONFLICT_CONTEXT;
         }
     }
 
-    class TestKeyConflictContext : IKeyConflictContext {
+    class TalismanKeyConflictContext : IKeyConflictContext {
         override fun isActive(): Boolean {
-            val player = DivineFavor.proxy.clientPlayer
-//            if (!net.minecraftforge.client.settings.KeyConflictContext.GUI.isActive())
-            //                return false;
-            return UtilPlayer.getHandWithItem(player) { it is ItemTalisman || it is ITalismanToolContainer } != null
+            if (!DivineFavor.proxy.hasClientPlayer)
+                return false
+            return UtilPlayer.getHandWithItem(DivineFavor.proxy.clientPlayer) { it is ItemTalisman || it is ITalismanToolContainer } != null
         }
 
         override fun conflicts(other: IKeyConflictContext): Boolean {
-            return false
-            //            return other == this;
-            //            return other == this || other == net.minecraftforge.client.settings.KeyConflictContext.IN_GAME;
+            return other == this || other == CONTAINER_CONFLICT_CONTEXT
         }
     }
 }
